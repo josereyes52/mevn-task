ea101fa5675e7aec8880b4462d8d25c59b227347
Jose Reyes <joseantonioreyes52@gmail.com>
2022-03-06 10:14:53 -0400

    Create (delete and post) methods

--- src/routes/task.js
+++ src/routes/task.js
@@ -4,12 +4,13 @@ const router = express.Router();
 // Import the model (task.js) to use its database functions.
 const Task = require("../models/Task");
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
     const tasks = await Task.find();
     res.json(tasks);
 });
 
-router.post("/", async (req, res) => {
+// Create a new task and save it to the database
+router.post('/', async (req, res) => {
     const { title, description } = req.body;
     const newTask = new Task({
         title,
@@ -18,6 +19,26 @@ router.post("/", async (req, res) => {
     await newTask.save();
     res.json({
         status: "Task saved"
+    });
+});
+
+// Update a task by its ID and save it to the database
+router.put('/:id', async (req, res) => {
+    const { title, description } = req.body;
+    await Task.findByIdAndUpdate(req.params.id, {
+        title,
+        description
+    });
+    res.json({
+        status: "Task updated"
+    });
+});
+
+// Delete a task by its ID
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({
+        status: "Task deleted"
     });
 });
 
